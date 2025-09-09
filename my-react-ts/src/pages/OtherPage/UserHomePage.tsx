103f9e84c53d077a938b8d0386eb15e68b5e1eef
DmitroKozachok <kozachokdima2006@gmail.com>
2025-09-09 21:29:12 +0300

    Test-4

--- my-react-ts/src/pages/OtherPage/UserHomePage.tsx
+++ my-react-ts/src/pages/OtherPage/UserHomePage.tsx
@@ -63,7 +63,7 @@ const UserHomePage: React.FC = () => {
             {/* Promo Section */}
             <section className="mt-24 px-6 py-16 bg-orange-50 dark:bg-gray-800 text-center">
                 <h3 className="text-2xl font-bold text-gray-800 dark:text-white mb-4">
-                    Знижка 10% на перше замовлення!
+                    Знижка 102% на перше замовлення!
                 </h3>
                 <p className="text-gray-600 dark:text-gray-300 mb-6">
                     Використай промокод <span className="font-semibold">WELCOME10</span> при оформленні замовлення.
